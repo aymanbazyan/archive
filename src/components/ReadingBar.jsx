f5059497f9957637b419684d16c5593b04963fff
Ayman Bazyan <103147442+aymanbazyan@users.noreply.github.com>
2025-03-15 14:24:12 +0200

    fixing scroll progress bar

--- src/components/ReadingBar.jsx
+++ src/components/ReadingBar.jsx
@@ -2,21 +2,21 @@ import { useReadingProgress } from "../hooks/useReadingProgress";
 import styled from "styled-components";
 
 const Nav = styled.nav`
-  position: sticky;
-  top: 32px;
+  height: 5px;
+  overflow: hidden;
   z-index: 50;
-  backdrop-filter: blur(3xl);
-  padding: 0.5rem 0;
+  position: sticky;
+  top: 44px;
 `;
 
 const ProgressBar = styled.span`
-  position: absolute;
-  bottom: 0;
   width: 100%;
-  height: 0.25rem;
+  height: 100%;
   background-color: var(--color-main); /* Equivalent to bg-yellow-400 */
   transition: transform 150ms;
   transform: ${({ completion }) => `translateX(${completion - 100}%)`};
+  position: absolute;
+  bottom: 0;
 `;
 
 export default function ReadingBar() {
